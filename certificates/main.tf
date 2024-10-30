66168871b7cd023f48856d7f307401515069ed52
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-10-30 15:57:28 +0530

    Valkey Certificates Creation for TLS

---
+++ certificates/main.tf
@@ -0,0 +1,87 @@
+// Certificate Authority to be used with Valkey Cluster
+resource "kubernetes_manifest" "valkey_ca" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "${var.valkey_ca_name}"
+      "namespace" = "${var.namespace}"
+      "labels" = {
+        "app"       = "valkey"
+        "component" = "ca"
+      }
+    }
+    "spec" = {
+      "isCA"       = true
+      "commonName" = "valkey-ca"
+      "secretName" = "valkey-ca-tls"
+      "duration"   = "70128h"
+      "privateKey" = {
+        "algorithm" = "ECDSA"
+        "size"      = 256
+      }
+      "issuerRef" = {
+        "name"  = "${var.cluster_issuer_name}"
+        "kind"  = "ClusterIssuer"
+        "group" = "cert-manager.io"
+      }
+    }
+  }
+}
+
+// Issuer for the valkey Cluster Namespace
+resource "kubernetes_manifest" "valkey_issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Issuer"
+    "metadata" = {
+      "name"      = "${var.valkey_issuer_name}"
+      "namespace" = "${var.namespace}"
+      "labels" = {
+        "app"       = "valkey"
+        "component" = "issuer"
+      }
+    }
+    "spec" = {
+      "ca" = {
+        "secretName" = "valkey-ca-tls"
+      }
+    }
+  }
+
+  depends_on = [kubernetes_manifest.valkey_ca]
+}
+
+// Certificate for valkey Cluster
+resource "kubernetes_manifest" "valkey_certificate" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "${var.valkey_certificate_name}"
+      "namespace" = "${var.namespace}"
+      "labels" = {
+        "app"       = "valkey"
+        "component" = "certificate"
+      }
+    }
+    "spec" = {
+      "dnsNames" = [
+        "*.valkey.valkey.svc.cluster.local",
+        "valkey-primary.valkey.svc.cluster.local",
+        "*.valkey-primary.valkey.svc.cluster.local",
+        "*.valkey-headless.valkey.svc.cluster.local",
+        "valkey-headless.valkey.svc.cluster.local",
+        "127.0.0.1",
+        "localhost",
+        "valkey",
+      ]
+      "secretName" = "valkey-tls"
+      "issuerRef" = {
+        "name" = "${var.valkey_issuer_name}"
+      }
+    }
+  }
+
+  depends_on = [kubernetes_manifest.valkey_issuer]
+}
---
+++ certificates/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "opentofu/kubernetes"
+      version = "2.32.0"
+    }
+  }
+
+  backend "kubernetes" {
+    secret_suffix = "certificates.valkey"
+  }
+}
+
+provider "kubernetes" {
+
+}
